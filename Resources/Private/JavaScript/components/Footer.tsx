05371e078fd1f5fbbc6f2fbb5e3d3e1a2eae5b9f
Sebastian Helzle <sebastian@helzle.it>
2022-04-26 09:58:03 +0200

    FEATURE: Show workspace count in module footer

--- Resources/Private/JavaScript/components/Footer.tsx
+++ Resources/Private/JavaScript/components/Footer.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useWorkspaces } from '../provider/WorkspaceProvider';
+import styled from 'styled-components';
+
+const WorkspaceCount = styled.div`
+    display: inline-block;
+    color: var(--textSubtleLight);
+    font-size: var(--generalFontSize);
+    line-height: var(--unit);
+    vertical-align: middle;
+    margin-left: var(--spacing-Full);
+`;
 
 const Footer: React.FC = () => {
-    const { setCreationDialogVisible } = useWorkspaces();
+    const { setCreationDialogVisible, workspaces } = useWorkspaces();
+
+    const workspaceCount = useMemo(() => {
+        return Object.values(workspaces).reduce(
+            (counts, workspace) => {
+                counts.total++;
+                if (workspace.isInternal) {
+                    counts.internal++;
+                } else {
+                    counts.private++;
+                }
+                return counts;
+            },
+            {
+                total: 0,
+                internal: 0,
+                private: 0,
+            }
+        );
+    }, [workspaces]);
 
     return (
         <div className="neos-footer">
@@ -13,6 +42,9 @@ const Footer: React.FC = () => {
             >
                 Create new workspace
             </button>
+            <WorkspaceCount>
+                {workspaceCount.total} workspaces ({workspaceCount.internal} public, {workspaceCount.private} private)
+            </WorkspaceCount>
         </div>
     );
 };

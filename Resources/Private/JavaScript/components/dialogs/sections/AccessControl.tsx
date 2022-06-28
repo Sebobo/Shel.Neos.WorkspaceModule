e8a0ef51865d389c60de4d9d1f1d45306ed3a766
Sebastian Helzle <sebastian@helzle.it>
2022-06-28 09:29:54 +0200

    TASK: Replace acl selectbox with checkboxes to improve UX
    Multi selection is quite unintuitive with selectboxes

--- Resources/Private/JavaScript/components/dialogs/sections/AccessControl.tsx
+++ Resources/Private/JavaScript/components/dialogs/sections/AccessControl.tsx
@@ -1,11 +1,22 @@
 import React, { ChangeEvent, useCallback, useRef, useState } from 'react';
 
 import { useWorkspaces } from '../../../provider/WorkspaceProvider';
-import { FormGroup, Icon, RadioLabel } from '../../presentationals';
+import { CheckBoxLabel, FormGroup, Icon, RadioLabel } from '../../presentationals';
+import styled from 'styled-components';
 
 type SectionProps = {
     workspace?: Workspace;
 };
+
+const AclList = styled.div`
+    max-height: 7rem;
+    overflow-x: hidden;
+    overflow-y: auto;
+
+    .neos & input[type='checkbox'] {
+        margin-top: 0;
+    }
+`;
 
 const AccessControl: React.FC<SectionProps> = ({ workspace }) => {
     const { translate, userList, userCanManageInternalWorkspaces } = useWorkspaces();
@@ -17,55 +28,59 @@ const AccessControl: React.FC<SectionProps> = ({ workspace }) => {
 
     // TODO: Allow setting an owner already during creation
     return workspace ? (
-        <FormGroup>
-            {!workspace.isPersonal && (
-                <label>
-                    {translate('workspace.owner.label', 'Owner')}
-                    <select
-                        name={'moduleArguments[workspace][owner]'}
-                        disabled={!userCanManageInternalWorkspaces}
-                        defaultValue={workspace?.owner?.id}
-                        ref={ownerField}
-                        onChange={updateOwner}
-                    >
-                        {Object.keys(userList).map((userId) => (
-                            <option key={userId} value={userId}>
-                                {userList[userId]}
-                            </option>
-                        ))}
-                    </select>
-                </label>
-            )}
-            <p>
-                <Icon icon="info-circle" style={{ color: 'var(--blue)', marginRight: '.5em' }} />
-                {workspace.isPersonal
-                    ? translate('workspace.visibility.isPersonal', 'This workspace is personal')
-                    : owner
-                    ? translate('workspace.visibility.private.info', 'This workspace is private')
-                    : translate('workspace.visibility.internal.info', 'This workspace is internal')}
-            </p>
-            {!workspace.isPersonal && owner && (
-                <FormGroup>
-                    <label>{translate('workspace.acl.label', 'Allow access for additional users:')}</label>
-                    <select
-                        name="moduleArguments[acl][]"
-                        multiple={true}
-                        defaultValue={workspace?.acl ? Object.values(workspace.acl).map((user) => user.id) : []}
-                        size={Math.min(5, Object.keys(userList).length)}
-                    >
-                        {Object.keys(userList).map((userId) =>
-                            userId && userId !== owner ? (
+        <>
+            <FormGroup>
+                {!workspace.isPersonal && (
+                    <label>
+                        {translate('workspace.owner.label', 'Owner')}
+                        <select
+                            name={'moduleArguments[workspace][owner]'}
+                            disabled={!userCanManageInternalWorkspaces}
+                            defaultValue={workspace?.owner?.id}
+                            ref={ownerField}
+                            onChange={updateOwner}
+                        >
+                            {Object.keys(userList).map((userId) => (
                                 <option key={userId} value={userId}>
                                     {userList[userId]}
                                 </option>
+                            ))}
+                        </select>
+                    </label>
+                )}
+                <p>
+                    <Icon icon="info-circle" style={{ color: 'var(--blue)', marginRight: '.5em' }} />
+                    {workspace.isPersonal
+                        ? translate('workspace.visibility.isPersonal', 'This workspace is personal')
+                        : owner
+                        ? translate('workspace.visibility.private.info', 'This workspace is private')
+                        : translate('workspace.visibility.internal.info', 'This workspace is internal')}
+                </p>
+            </FormGroup>
+            {!workspace.isPersonal && owner && (
+                <FormGroup>
+                    <label>{translate('workspace.acl.label', 'Allow access for additional users:')}</label>
+                    <AclList>
+                        {Object.keys(userList).map((userId) =>
+                            userId && userId !== owner ? (
+                                <CheckBoxLabel key={userId}>
+                                    <input
+                                        type="checkbox"
+                                        className="neos-checkbox"
+                                        value={userId}
+                                        name="moduleArguments[acl][]"
+                                        defaultChecked={Object.values(workspace.acl).some((user) => user.id === userId)}
+                                    />
+                                    {userList[userId]}
+                                </CheckBoxLabel>
                             ) : (
                                 ''
                             )
                         )}
-                    </select>
+                    </AclList>
                 </FormGroup>
             )}
-        </FormGroup>
+        </>
     ) : (
         <FormGroup>
             <label className="neos-control-label">{translate('workspace.visibility.label', 'Visibility')}</label>
--- Resources/Private/JavaScript/components/presentationals/CheckBoxLabel.ts
+++ Resources/Private/JavaScript/components/presentationals/CheckBoxLabel.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-const RadioLabel = styled.label`
+const CheckBoxLabel = styled.label`
     flex-direction: row;
     align-items: center;
     gap: 0.6rem;
     cursor: pointer;
 `;
 
-export default RadioLabel;
+export default CheckBoxLabel;

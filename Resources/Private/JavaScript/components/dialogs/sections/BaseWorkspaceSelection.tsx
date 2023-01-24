52c439d6a0a34c1a0190b24967e769a67650c613
Sebastian Helzle <sebastian@helzle.it>
2023-01-24 10:57:55 +0100

    BUGFIX: Sort baseworkspace options and make „live“ always appear first

--- Resources/Private/JavaScript/components/dialogs/sections/BaseWorkspaceSelection.tsx
+++ Resources/Private/JavaScript/components/dialogs/sections/BaseWorkspaceSelection.tsx
@@ -10,40 +10,44 @@ const BaseWorkspaceSelection: React.FC<SectionProps> = ({ workspace }) => {
     const { translate, baseWorkspaceOptions, workspaces } = useWorkspaces();
 
     const selectableBaseWorkspaceNames = useMemo(() => {
-        const workspaceNames = Object.keys(baseWorkspaceOptions);
-        if (!workspace) {
-            return workspaceNames;
-        }
-        // Check if the selection of the workspace would create a cycle
-        return workspaceNames.filter((workspaceName) => {
-            // The live workspace is always selectable
-            if (workspaceName === 'live') {
-                return true;
-            }
-            // Don't allow to select the current workspace as base workspace
-            if (workspaceName === workspace.name) {
-                return false;
-            }
-            const workspaceToCheck = workspaces[workspaceName];
-
-            // If the workspace to check is not accessible, it is not selectable
-            if (!workspaceToCheck) {
-                return false;
-            }
-
-            // Allow to selecting workspaces as base workspace if they have no base workspace
-            if (!workspaceToCheck.baseWorkspace) {
-                return true;
-            }
-            // Don't allow to selecting workspaces that have the current workspace in their base workspace chain
-            let baseWorkspaceName = workspaceToCheck.baseWorkspace?.name;
-            while (baseWorkspaceName && baseWorkspaceName !== 'live') {
-                if (baseWorkspaceName === workspace.name) {
+        let workspaceNames = Object.keys(baseWorkspaceOptions);
+        if (workspace) {
+            // Check if the selection of the workspace would create a cycle
+            workspaceNames = workspaceNames.filter((workspaceName) => {
+                // The live workspace is always selectable
+                if (workspaceName === 'live') {
+                    return true;
+                }
+                // Don't allow to select the current workspace as base workspace
+                if (workspaceName === workspace.name) {
                     return false;
                 }
-                baseWorkspaceName = workspaces[baseWorkspaceName].baseWorkspace?.name;
-            }
-            return true;
+                const workspaceToCheck = workspaces[workspaceName];
+
+                // If the workspace to check is not accessible, it is not selectable
+                if (!workspaceToCheck) {
+                    return false;
+                }
+
+                // Allow to selecting workspaces as base workspace if they have no base workspace
+                if (!workspaceToCheck.baseWorkspace) {
+                    return true;
+                }
+                // Don't allow to selecting workspaces that have the current workspace in their base workspace chain
+                let baseWorkspaceName = workspaceToCheck.baseWorkspace?.name;
+                while (baseWorkspaceName && baseWorkspaceName !== 'live') {
+                    if (baseWorkspaceName === workspace.name) {
+                        return false;
+                    }
+                    baseWorkspaceName = workspaces[baseWorkspaceName].baseWorkspace?.name;
+                }
+                return true;
+            });
+        }
+        return workspaceNames.sort((a, b) => {
+            if (a === 'live') return -1;
+            if (b === 'live') return 1;
+            return baseWorkspaceOptions[a].localeCompare(baseWorkspaceOptions[b]);
         });
     }, [workspace, baseWorkspaceOptions, workspaces]);
 
